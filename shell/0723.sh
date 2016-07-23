f30cedb2e75265382302741c783547d097eb7baf
honpey <houpengyang0901@126.com>
2016-07-23 22:25:48 +0800

    sed, filter the pattern lines by // just as awk

--- shell/0723.sh
+++ shell/0723.sh
@@ -13,4 +13,22 @@ cat alpha.txt | sed "/\([^ ]\|[^;] \)SP90 alpha inhibitor/d"
 
 #Attention: sed中小括号的转义
 
+#--------------------------------------#
+#  ------   sed高级用法 --------       #
+#--------------------------------------#
+
+# 数据库中经常会有这样的操作: create table XXXXX
+# 使用sed把所有的 XXXXX 变成 XXXXX_20160628
+
+# 方案1:(比较菜鸟, 只是用到了基本的\1的替换的功能 )
+# sed -r "s/create table 
+#             ([0-9a-zA-z])/create table \1_20160628/g" table.txt
+
+# 方案2: (提前对这样的行做了一个筛选)
+sed '/create table .*/s/ (/_20160628 (/' table.txt
+
+
+# 方案3:
+sed -r 's/create table \w+/&_20160628/' table.txt
+
 
