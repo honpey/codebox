10334bd0970e9306fa32a4ffe408974ebd7d28e9
honpey <houpengyang0901@126.com>
2016-07-26 08:16:24 +0800

    sed usage

--- shell/0724.sh
+++ shell/0724.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 # awk 'BEGIN{RS="]";count=0;}{gsub("\n","",$0); print substr($0,2,length($0));}` file
+
+sed '/\[/{:a;N;/\n\[/!{s/\n//;ta};P;D}'
+
+sed '/\[/{:a;N;/\n\[/!{s/\n//;ta};P;D}'
+
+sed '/\[/{:a;N;/\n\[/!{s/\n\[//g;ta}};P;D;'
+
+sed '/^\[/{:a;N;/^\[/!{s/\n\[//g;ta}};P;D;'
+
