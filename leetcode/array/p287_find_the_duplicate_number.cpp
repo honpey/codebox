dc9dfcbb1bc451416739863339e92c359f0e4c67
honpey <houpengyang0901@126.com>
2017-05-13 10:34:26 +0800

    leetcode: array: Find the Duplicate Number

--- leetcode/array/p287_find_the_duplicate_number.cpp
+++ leetcode/array/p287_find_the_duplicate_number.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <unistd.h>
 
 using namespace std;
 
@@ -34,31 +35,64 @@ int findDuplicate1(vector<int>& nums) { /* fail at 2,2,3,4 */
 }
 /* 二分法是有问题的 */
 
+/* 看网上攻略，从环入手分析 */
+/*
+ * 比如下面的数组：3 3 1 2 4
+ * 首先，最好的办法就是各就各位，对于这种1-n类型的数组，让它们
+ * 各就各位就是最好的办法了，但是现在题目要求不能够调整元素的位置
+ * 只能远远地用环的方法处理了：
+ *
+ * 0 1 2 3 4 --- 共有5个元素，但是里面的值是1,2,3,4
+ * 3 3 1 2 4
+ * 
+ * 0->3 (pos:2)
+ * 2->1 (pos:0)
+ * 0->3 (pos:2) 已经达到一个环了，那么0,2这个位置相当于是安全的了
+ * .......
+ * a[1]=3 (pos:2)
+ * a[2]=1 (pos:0)
+ * a[0]=3 (pos:2)
+ * a[2]=1 (pos:0)
+ * a[0]=3 (pos:2) 是一个β类型的环，但是怎么找到这个出事
+ * ......   
+ * a[2]=1 (pos:0)
+ * a[0]=3 (pos:2)
+ * a[2]=1 (pos:0)
+ * a[0]=3 
+ * ......
+ * a[3]=2 (pos:1)
+ * a[1]=3 (pos:2)
+ * a[2]=1 (pos:0)
+ * a[0]=3 (pos:2)
+ *
+ * 上面的分析都是不对的，关键点：如何利用好a[0]这个位置！！！！
+ * 
+ */
 
 int findDuplicate(vector<int>& nums)
 {
-	int i;
-	int start =0, end=nums.size()-1;
-	int mid, precount = 0, poscount = 0;
+	int fast = 0, slow = 0;
 	if (nums.size() <= 0)
 		return -1;
-	while (start < end) {
-		mid = (start + end)/2;
-		precount = 0; poscount = 0;
-		for (i = 0; i < nums.size(); i++) {
-			if (nums[i] < nums[mid]) {
-				precount++;
-			}
-			
-		}
-	}
-	return 1; 
+	do {
+		fast = nums[nums[fast]];
+		slow = nums[slow];
+	} while (fast != slow);
+
+	fast = 0;
+	do {
+		if (nums[fast] == nums[slow])
+			break;
+		fast = nums[fast];
+		slow = nums[slow];
+	} while(1);
+	return nums[fast];
 }
 
 int main()
 {
-	int a[] = {2,2,3,4};
+	int a[] = {1,1};
 	vector<int> iv(a, a+sizeof(a)/sizeof(int));
-	cout<<findDuplicate1(iv)<<endl;
+	cout<<findDuplicate(iv)<<endl;
 	return 0;
 }
