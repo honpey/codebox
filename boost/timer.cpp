a4a63fff2a735fdad807c65388006456ed299a77
honpey <houpengyang0901@126.com>
2017-06-03 23:39:49 +0800

    boost: timer

---
+++ boost/timer.cpp
@@ -0,0 +1,15 @@
+#include <boost/timer.hpp>
+#include <iostream>
+using namespace boost;
+using namespace std;
+
+int main()
+{
+	timer t;
+	cout<< "max timespan:"
+		<<t.elapsed_max()/3600<<"h"<<endl;
+	cout<<"min timespen:"
+		<<t.elapsed_min()<<"s"<<endl;
+	cout<<"now time elpsed:"
+		<<t.elapsed()<<"s"<<endl;
+}
