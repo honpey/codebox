ee15fdbca7f8b255ebf783075ae63c323978bb64
honpey <houpengyang0901@126.com>
2017-11-05 20:21:10 +0800

    use PROT_WRITE lonely

--- mmap/anon.c
+++ mmap/anon.c
@@ -6,12 +6,14 @@ int main()
 {
 	int i, fd, pid;
 	char *ptr;
-
-	ptr = mmap(NULL, 4096*4, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0); /*  看下子进程是否能够访问 */
-	printf("ptr:%p\n", ptr);
+	ptr = mmap(NULL, 4096*4, PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0); /*  看下子进程是否能够访问 */
+	sleep(4);
 	ptr[0] = 1;
-	ptr[4096] = 2;
-	ptr[4096*2] = 3;
-	ptr[4096*3] = 4;
+	ptr[4096] = 1;
+	ptr[4096*2] = 1;
+	ptr[4096*3] = 1;
+	printf("anonymous down\n");
+
+	while(1);
 
 }
