de22f94bba9fa73539639d2dad4b1cc0863447d3
honpey <houpengyang0901@126.com>
2017-11-09 23:07:39 +0800

    how to parse C++ string in stap

---
+++ stap/cpp/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class A {
+public:
+	A(){};
+	~A(){};
+	void setName(string a);
+	string getName();
+	string name;
+};
+
+void A::setName(string a) {
+	name = a;
+}
+string A::getName() {
+	return name;
+}
+
+int main()
+{
+	A *a = new A();
+	while (1) {
+		a->setName("xiaohong");
+		cout<<a->getName()<<endl;
+		int i = 0;
+		while (i++ < 0xfffffff);
+	}
+}
