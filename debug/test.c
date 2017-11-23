6ed8be292671c17499f5856ee7a278301792d5c6
honpey <houpengyang0901@126.com>
2017-11-24 00:09:04 +0800

    insert a probe inside function

---
+++ debug/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int func(int a) {
+	int i = 1;
+	int z = 0;
+	for (i = 0; i < 10; i++) {
+		int j = 0;
+		for (j = 0; j < 100; j++) {
+			z++;
+		}
+	}
+	return z + a;
+}
+int main() {
+	while(1) {
+		func(100);
+		sleep(1);
+	}
+}
