588723442fac6d88ac218ca4599f23cf6daeaa93
honpey <houpengyang0901@126.com>
2019-03-16 18:42:07 +0800

    add class parsing info

---
+++ python.code/class2/hi4/base.py
@@ -0,0 +1,10 @@
+class People:
+    name = "people"
+    age = 19
+    def __init__(self, name, age):
+        self.name=name         
+        self.age = age
+    def setAge(self, name):
+        self.name=name
+
+
--- python.code/main.py
+++ python.code/main.py
@@ -1,8 +1,22 @@
-from class2.hi3 import base
+from class2.hi2 import base
+from class2.hi2.base import hi
+from class2.hi4 import base
 if __name__ == '__main__':
+    """
     xiaohong = base.hi("xiaohong")
     print xiaohong.getName()
     xiaoming = base.hi2("xiaoming", 12)
     print xiaoming.setAge(13)
     print xiaoming.getNameAge()
     print "hello world"
+
+    xiaolv = hi("xiaolv")
+    print xiaolv
+    """
+    base1 = base.People
+    peo = base.People("xiaolv", 12)
+    print peo.name
+    print peo.age
+    print base1.name
+    print base1.age
+
