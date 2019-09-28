01bb417729a4f7788f2d8974b55f708d662ac1fe
Treestein Hon <honpey@gmail.com>
2019-09-28 17:42:02 +0800

    Add perf example

---
+++ perf/gen.sh
@@ -0,0 +1,12 @@
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3173 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3913 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3914 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3915 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3916 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3917 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3918 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3919 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3920 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3921 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3922 &
+ sudo perf top -e branch-instructions -e branch-misses -e bus-cycles -e cache-misses -e cache-references -e cpu-cycles -e instructions -e ref-cycles -p 3991 &
