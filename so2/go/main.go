7f5b270f2c32e0ce9da8770bcfe131cfb0390e94
TreeHon <honpey@gmail.com>
2021-01-16 17:58:33 +0800

    add so2/go/main.go

--- so2/go/main.go
+++ so2/go/main.go
@@ -17,7 +17,6 @@ package main
 //   newFunc = (int (*)(char *a, char *b))f;
 //   return newFunc(key, cert);
 // }
-
 import "C"
 
 import "fmt"
@@ -25,24 +24,23 @@ import "fmt"
 func main() {
   fmt.Println("hello world")
   handle := C.dlopen(C.CString("./liba.so"), C.RTLD_LAZY)
-  funA := C.dlsym(handle, C.CString("merge2"))
+  funA := C.dlsym(handle, C.CString("merge"))
 
-  var str string
   a1 := "AAAA"
   b1 := "BBBB"
-  //u := C.CString(str)
 
-  fmt.Println("%v %v\n", handle, funA)
-  for i := 0; i < 10; i++ {
-    ret := C.bridge_func(funA, C.CString(a1), C.CString(b1))
-    fmt.Printf("Result %v -> %v|\n", ret, str)
-  }
-
+  var p *C.char = new(C.char)
   /*
   fmt.Println("%v %v\n", handle, funA)
   for i := 0; i < 10; i++ {
-    ret := C.bridge_func(funA, C.CString(a1), C.CString(b1), &u)
+    ret := C.bridge_func2(funA, C.CString(a1), C.CString(b1))
     fmt.Printf("Result %v -> %v|\n", ret, str)
   }
   */
+
+  fmt.Println("%v %v\n", handle, funA)
+  for i := 0; i < 10; i++ {
+    ret := C.bridge_func(funA, C.CString(a1), C.CString(b1), &p)
+    fmt.Printf("Result %v -> %v|\n", ret, p)
+  }
 }
